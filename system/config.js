d4fc26885ddeae647141b7f6ab53a4133d738907
Jarsepay <jarsepay@gmail.com>
2024-10-26 08:54:57 +0800

    Update config.js

--- system/config.js
+++ system/config.js
@@ -16,7 +16,6 @@ global.owner = [
 
 // Info
 global.info = {
-    pairingNumber: '', // Please fill your number starting with your country code, example: '6282148864989'
     namaowner: 'Jarsépay',
     nomorowner: '6282148864989',
     packname: 'Sticker by ',
